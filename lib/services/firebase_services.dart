012f360cc029b54879027dad5a00cff28aef54a9
mhebd <mhemonhossain@gmail.com>
2024-09-28 23:14:37 +0600

    login and get user info method setup done

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -7,5 +7,31 @@ class FirebaseServices {
   FirebaseStorage storage = FirebaseStorage.instance;
   FirebaseFirestore fireStore = FirebaseFirestore.instance;
 
+  Map? currentUser;
+
   FirebaseServices();
+
+  // Login a user;
+  Future<bool> loginUser(
+      {required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      currentUser = await getUserInfo(uid: userCredential.user!.uid);
+      if (userCredential.user != null) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+  // Get user info
+  Future<Map> getUserInfo({required String uid}) async {
+    DocumentSnapshot userInfo =
+        await fireStore.collection('users').doc(uid).get();
+    return userInfo.data() as Map;
+  }
 }
